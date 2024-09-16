0a001920063337b1e5772fd496607c77299e0089
Willuv <Will.Uvlin@gmail.com>
2024-09-16 17:23:42 -0400

    Begin Implementation of Command Classes and Keyboard input

---
+++ LinkUpCommand.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legend_of_the_Power_Rangers
+{
+    public class LinkUpCommand : ICommand
+    {
+        private Game1 game;
+        public LinkUpCommand(Game1 game) {
+            
+        }
+        public void Execute()
+        {
+        }
+    }
+}
