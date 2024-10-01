251352481f0542a94d763192a6eb8a43193965bf
Willuv <Will.Uvlin@gmail.com>
2024-09-30 23:24:35 -0400

    move files into folders

---
+++ InputCommands/LinkItem2Command.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legend_of_the_Power_Rangers
+{
+    public class LinkItem2Command : ICommand
+    {
+        private readonly LinkStateMachine stateMachine;
+        private readonly LinkItemFactory linkItemFactory;
+        public LinkItem2Command(LinkStateMachine stateMachine, LinkItemFactory linkItemFactory)
+        {
+            this.stateMachine = stateMachine;
+            this.linkItemFactory = linkItemFactory;
+        }
+        public void Execute()
+        {
+            this.stateMachine.ChangeAction(LinkStateMachine.LinkAction.Item);
+            this.linkItemFactory.CreateItem(Item.ItemType.Arrow);
+        }
+    }
+}
---
+++ InputCommands/LinkItem4Command.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legend_of_the_Power_Rangers
+{
+    public class LinkItem4Command : ICommand
+    {
+        private readonly LinkStateMachine stateMachine;
+        private readonly LinkItemFactory linkItemFactory;
+        public LinkItem4Command(LinkStateMachine stateMachine, LinkItemFactory linkItemFactory)
+        {
+            this.stateMachine = stateMachine;
+            this.linkItemFactory = linkItemFactory;
+        }
+        public void Execute()
+        {
+            this.stateMachine.ChangeAction(LinkStateMachine.LinkAction.Item);
+            this.linkItemFactory.CreateItem(Item.ItemType.Boomerang);
+        }
+    }
+}
