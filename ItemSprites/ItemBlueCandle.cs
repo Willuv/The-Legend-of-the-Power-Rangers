733adf4d5e01831c0ac77cdcb0aa152130c756d9
maxmuccillo <maxmuccillo@gmail.com>
2024-10-18 15:02:25 -0400

    fixed get set for items and scaled blocks and items

--- ItemSprites/ItemBlueCandle.cs
+++ ItemSprites/ItemBlueCandle.cs
@@ -8,14 +8,14 @@ namespace Legend_of_the_Power_Rangers
 {
     public class ItemBlueCandle : IItem
     {
-        public Rectangle position = new Rectangle(370, 300, 16, 16);
+        public Rectangle destinationRectangle = new Rectangle(370, 300, 32, 32);
 
-        public Rectangle rectangle = new Rectangle(120, 0, 16, 16);
+        public Rectangle sourceRectangle = new Rectangle(120, 0, 16, 16);
 
         public Rectangle DestinationRectangle
         {
-            get {return rectangle;}
-            set {rectangle = value;}
+            get {return destinationRectangle; }
+            set { destinationRectangle = value;}
         }
 
         public ObjectType ObjectType { get { return ObjectType.Item; } }
@@ -27,7 +27,7 @@ namespace Legend_of_the_Power_Rangers
         }
         public void Draw(Texture2D texture, SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(texture, position, rectangle, Color.White);
+            spriteBatch.Draw(texture, destinationRectangle, sourceRectangle, Color.White);
         }
     }
 }
--- ItemSprites/ItemBow.cs
+++ ItemSprites/ItemBow.cs
@@ -8,13 +8,13 @@ namespace Legend_of_the_Power_Rangers
 {
     public class ItemBow : IItem
     {
-        public Rectangle position = new Rectangle(370, 300, 16, 16);
+        public Rectangle destinationRectangle = new Rectangle(370, 300, 32, 32);
 
-        public Rectangle rectangle = new Rectangle(320, 0, 16, 16);
+        public Rectangle sourceRectangle = new Rectangle(320, 0, 16, 16);
         public Rectangle DestinationRectangle
         {
-            get {return rectangle;}
-            set {rectangle = value;}
+            get {return destinationRectangle; }
+            set { destinationRectangle = value;}
         }
 
         public ObjectType ObjectType { get { return ObjectType.Item; } }
@@ -26,7 +26,7 @@ namespace Legend_of_the_Power_Rangers
         }
         public void Draw(Texture2D texture, SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(texture, position, rectangle, Color.White);
+            spriteBatch.Draw(texture, destinationRectangle, sourceRectangle, Color.White);
         }
     }
 }
