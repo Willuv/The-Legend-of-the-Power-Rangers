f37183d1af7d187ab5c002c4e627f0beac23815e
Christian Colon <colonchristian224@gmail.com>
2024-11-14 10:54:25 -0500

    removed mouse from pause state

--- GameStateMachine.cs
+++ GameStateMachine.cs
@@ -237,7 +237,6 @@ namespace Legend_of_the_Power_Rangers
                     break;
                 case GameState.Paused:
                     keyboardController.Update();
-                    mouseController.Update();
                     hud.Update(level.currentRoom);
                     // Handle paused state update
                     break;
